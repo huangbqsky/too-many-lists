d639a094d8510d9228bf283738c4136549326b8e
huangbqsky <huangbqsky@gmail.com>
2023-02-28 17:28:26 +0800

    一个功能残缺的双向链表

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,4 @@ pub mod first;  // 不太优秀的单向链表：栈
 pub mod second; // 还可以的单向链表：单所有权 Option + Box
 pub mod thrid; // 持久化单向链表：共享所有权 Rc
 
-pub mod four; // 一个不咋滴的双向链表 (功能残缺的，只实现了IntoIter， 未实现 Iter 和 IterMut)
+pub mod fourth; // 一个不咋滴的双向链表 (功能残缺的，只实现了IntoIter， 未实现 Iter 和 IterMut)
