372c42ed5285aa57de912e63df9aec9be4a58483
huangbqsky <huangbqsky@gmail.com>
2023-02-28 14:38:50 +0800

    增加peek和peek_mut方法

--- src/second.rs
+++ src/second.rs
@@ -1,4 +1,3 @@
-
 // 类型别名，type alias
 type Link<T> = Option<Box<Node<T>>>;
 
@@ -20,10 +19,10 @@ impl<T> List<T> {
     // 添加一个值到链表
     pub fn push(&mut self, val: T) {
         let new_node = Box::new(Node {
-             elem: val, 
-             next: self.head.take(),
-         });
-         self.head = Some(new_node);
+            elem: val,
+            next: self.head.take(),
+        });
+        self.head = Some(new_node);
     }
 
     // 取出一个数据
@@ -32,6 +31,14 @@ impl<T> List<T> {
             self.head = node.next;
             node.elem
         })
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
     }
 }
 
@@ -76,5 +83,22 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        list.peek_mut().map(|value| *value = 42);
+
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
     }
 }
