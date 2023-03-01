8123d3ca9fc8af596853e26fa8993c24002967e2
huangbqsky <huangbqsky@gmail.com>
2023-03-01 21:16:32 +0800

    unsafe 单链表

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,5 @@ pub mod second; // 还可以的单向链表：单所有权 Option + Box
 pub mod thrid; // 持久化单向链表：共享所有权 Rc
 
 pub mod fourth; // 一个不咋滴的双向链表 (功能残缺的，只实现了IntoIter， 未实现 Iter 和 IterMut)
+
+pub mod fifth; //  一个不错的的unsafe单向链表: 使用裸指针和 unsafe 代码实现一个单向链表
